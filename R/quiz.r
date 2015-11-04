3af0540a1611f8a050ff5066f539776b5dbfdd2d
Sebastian Kranz <sebastian.kranz@uni-ulm.de>
2015-11-04 17:49:19 +0100

    update quiz

--- R/quiz.r
+++ R/quiz.r
@@ -94,44 +94,81 @@ award:
   
 }
 
-shinyQuiz = function(id=paste0("quiz_",sample.int(10e10,1)), yaml, quiz.handler=NULL, add.handler=TRUE, single.check.btn=TRUE) {
-  restore.point("shinyQuiz")
-  
-  qu = parse.quiz.yaml(yaml, id)
-  
-  qu$single.check.btn = single.check.btn
-  if (qu$single.check.btn) {
-     qu$checkBtnId = paste0(qu$id,"__checkBtn")
+quizDefaults = function(lang="en") {
+  if (lang=="de") {
+    list(
+      success = "richtig!",
+      failure= "leider falsch",
+      success_color = "blue",
+      failure_color = "red"
+    )
+  } else {
+    list(
+      success = "correct",
+      failure= "not correct",
+      success_color = "blue",
+      failure_color = "red"
+    )
   }
-  
-  qu$parts = lapply(seq_along(qu$parts), function(ind) init.quiz.part(qu$parts[[ind]],ind,qu))
-  np = length(qu$parts)
-  qu$state = as.environment(list(part.solved=rep(FALSE,np), solved=FALSE))
-
-  
-  if (add.handler)
-    add.quiz.handlers(qu, quiz.handler)
-  qu
 }
 
-parse.quiz.yaml = function(yaml, id=paste0("quiz_",sample.int(10e10,1))) {
-  restore.point("parse.quiz.yaml")
-  library(YamlObjects)
-  qu = read.yaml(text=yaml)
-  
+#' Create a shiny quiz widget
+#'
+#' @param id the id of the quiz
+#' @param qu a list that contains the quiz fields as would have
+#'        been parsed by read.yaml from package YamlObjects
+#' @param yaml alternatively to qu, is yaml a string that specifies the quiz
+#' @param quiz.handler a function that will be called if the quiz is checked.
+#'        The boolean argument solved is TRUE if the quiz was solved
+#'        and otherwise FALSE
+shinyQuiz = function(id=paste0("quiz_",sample.int(10e10,1)),qu=NULL, yaml,  quiz.handler=NULL, add.handler=TRUE, single.check.btn=TRUE, defaults=quizDefaults(lang=lang), lang="en") {
+  restore.point("shinyQuiz")
+
+  if (is.null(qu)) {
+    yaml = enc2utf8(yaml)
+    qu = mark_utf8(read.yaml(text=yaml))
+  }
+
   if (is.null(qu[["id"]])) {
     qu$id = id
   }
   if (is.null(qu$parts)) {
     qu$parts = list(qu)
   }
-  
-  qu    
+
+
+  qu$single.check.btn = single.check.btn
+  if (qu$single.check.btn) {
+     qu$checkBtnId = paste0(qu$id,"__checkBtn")
+  }
+
+  qu$parts = lapply(seq_along(qu$parts), function(ind) init.quiz.part(qu$parts[[ind]],ind,qu))
+  np = length(qu$parts)
+  qu$state = as.environment(list(part.solved=rep(FALSE,np), solved=FALSE))
+
+  qu$ui = quiz.ui(qu)
+
+  if (add.handler)
+    add.quiz.handlers(qu, quiz.handler)
+  qu
 }
 
-init.quiz.part = function(part=qu$parts[[part.ind]], part.ind=1, qu, has.check.btn=!qu$single.check.btn) {
+init.quiz.part = function(part=qu$parts[[part.ind]], part.ind=1, qu, has.check.btn=!qu$single.check.btn, defaults=quizDefaults()) {
   restore.point("init.quiz.part")
-  
+
+  part = copy.into.missing.fields(dest=part, source=defaults)
+
+  if (!is.null(part[["sc"]])) {
+    part$choices = part$sc
+    part$multiple = FALSE
+    #part$type = "sc"
+  } else if (!is.null(part[["mc"]])) {
+    part$choices = part$mc
+    part$multiple = TRUE
+    #part$type = "mc"
+  }
+
+
   if (!is.null(part$choices)) {
     correct.choices = which(str.ends.with(part$choices,"*"))
     if (is.null(part$multiple)) {
@@ -155,21 +192,16 @@ init.quiz.part = function(part=qu$parts[[part.ind]], part.ind=1, qu, has.check.b
     }
   } else {
     stop(paste0("The quiz with question ", part$question, " has neither defined the field 'answer' nor the field 'choices'."))
-  } 
-  
-  if (is.null(part$success)) {
-    part$success = "<p><font color='blue'>correct</font></p>"
-  } else {
-    part$success =  markdownToHTML(text=part$success,encoding = "UTF-8", fragment.only=TRUE)
   }
-  if (is.null(part$failure)) {
-    part$failure = "<p><font color='red'>not correct</font></p>"
-  } else {
-    part$failure =  markdownToHTML(text=part$failure,encoding = "UTF-8", fragment.only=TRUE)
-  }
-  
-  
-  part$id = paste0(qu$id,"__part", part.ind) 
+
+
+  txt = colored.html(part$success, part$success_color)
+  part$success =  markdownToHTML(text=txt,encoding = "UTF-8", fragment.only=TRUE)
+
+  txt = colored.html(part$failure, part$failure_color)
+  part$failure =  markdownToHTML(text=txt,encoding = "UTF-8", fragment.only=TRUE)
+
+  part$id = paste0(qu$id,"__part", part.ind)
   part$answerId = paste0(part$id,"__answer")
   if (has.check.btn) {
     part$checkBtnId = paste0(part$id,"__checkBtn")
@@ -179,38 +211,63 @@ init.quiz.part = function(part=qu$parts[[part.ind]], part.ind=1, qu, has.check.b
   part$resultId = paste0(part$id,"__resultUI")
   part$ui = quiz.part.ui(part)
   part$solved = FALSE
-  
+
   part
 }
 
-quiz.ui = function(qu) {
+quiz.ui = function(qu, solution=FALSE) {
   restore.point("quiz.ui")
   pli = lapply(seq_along(qu$parts), function(i) {
     part = qu$parts[[i]]
-    if (i < length(qu$parts)) return(list(part$ui,hr()))
-    part$ui
+    if (i < length(qu$parts)) {
+      hr = hr()
+    } else {
+      hr = NULL
+    }
+
+    if (solution) {
+      if (is.null(part$sol.ui)) {
+        part$sol.ui = quiz.part.ui(part, solution=TRUE)
+      }
+      return(list(part$sol.ui,hr))
+    } else {
+      return(list(part$ui,hr))
+    }
   })
   if (!is.null(qu$checkBtnId)) {
     pli = c(pli, list(actionButton(qu$checkBtnId,label = "check")))
   }
-  
+
   pli
 }
 
-quiz.part.ui = function(part) {
+quiz.part.ui = function(part, solution=FALSE, add.button=!is.null(part$checkBtnId)) {
   head = list(
     HTML(paste0("<p>",part$question,"</p>"))
   )
-  if (part$type=="numeric") {
-    answer = numericInput(part$answerId, label = "",value = NULL)  
-  } else if (part$type =="text") {
-    answer = textInput(part$answerId, label = "",value = "")  
-  } else if (part$type=="mc") {
-    answer = checkboxGroupInput(part$answerId, "",part$choices)
-  } else if (part$type=="sc") {
-    answer = radioButtons(part$answerId, "",part$choices, selected=NA)
+  if (solution) {
+    if (part$type=="numeric") {
+      answer = numericInput(part$answerId, label = "",value = part$answer)
+    } else if (part$type =="text") {
+      answer = textInput(part$answerId, label = "",value = part$answer)
+    } else if (part$type=="mc") {
+      answer = checkboxGroupInput(part$answerId, "",part$choices,selected = part$answer)
+    } else if (part$type=="sc") {
+      answer = radioButtons(part$answerId, "",part$choices, selected=part$answer)
+    }
+  } else {
+    if (part$type=="numeric") {
+      answer = numericInput(part$answerId, label = "",value = NULL)
+    } else if (part$type =="text") {
+      answer = textInput(part$answerId, label = "",value = "")
+    } else if (part$type=="mc") {
+      answer = checkboxGroupInput(part$answerId, "",part$choices)
+    } else if (part$type=="sc") {
+      answer = radioButtons(part$answerId, "",part$choices, selected=NA)
+    }
   }
-  if (!is.null(part$checkBtnId)) {
+
+  if (add.button) {
     button = actionButton(part$checkBtnId,label = "check")
   } else {
     button = NULL
@@ -218,8 +275,15 @@ quiz.part.ui = function(part) {
   list(head,answer,button, uiOutput(part$resultId))
 }
 
-add.quiz.handlers = function(qu, quiz.handler=NULL){
+
+add.quiz.handlers = function(qu, quiz.handler=NULL, id=qu$id){
   restore.point("add.quiz.handlers")
+  app = getApp()
+  if (is.null(app)) {
+    cat("\nCannot add quiz handlers since no shinyEvents app object is set.")
+    return()
+  }
+
   if (!qu$single.check.btn) {
     for (part.ind in seq_along(qu$parts)) {
       part = qu$parts[[part.ind]]
@@ -232,22 +296,23 @@ add.quiz.handlers = function(qu, quiz.handler=NULL){
 
 click.check.quiz = function(app=getApp(), part.ind, qu, quiz.handler=NULL, ...) {
   restore.point("click.check.quiz")
-  
+
   # check all parts
   if (part.ind == 0) {
     for (part.ind in seq_along(qu$parts))
       click.check.quiz(app=app, part.ind=part.ind,qu=qu, quiz.handler=NULL)
-    
+
     if (!is.null(quiz.handler)) {
       quiz.handler(app=app, qu=qu, part.ind=0, part.correct=NA, solved=qu$state$solved)
     }
     return(qu$state$solved)
   }
-  
+
   part = qu$parts[[part.ind]]
   answer = getInputValue(part$answerId)
-  
-  
+  restore.point("click.check.quiz.inner")
+
+
   if (part$type =="numeric") {
     answer = as.numeric(answer)
     correct = is.true(abs(answer-part$answer)<part$roundto)
@@ -263,9 +328,9 @@ click.check.quiz = function(app=getApp(), part.ind, qu, quiz.handler=NULL, ...) 
   }
   qu$state$part.solved[part.ind] = correct
   qu$state$solved = all(qu$state$part.solved)
-  
+
   if (!is.null(quiz.handler)) {
     quiz.handler(app=app, qu=qu, part.ind=part.ind, part.correct=correct, solved=qu$state$solved)
   }
-  
+
 }
--- R/tools_rtutor.r
+++ R/tools_rtutor.r
@@ -1,3 +1,31 @@
+copy.into.missing.fields = function(dest, source) {
+  restore.point("copy.into.empty.fields")
+
+  new.fields = setdiff(names(source), names(dest))
+  dest[new.fields] = source[new.fields]
+  dest
+}
+
+
+colored.html = function(txt, color="blue") {
+  if (is.null(color)) return(txt)
+  paste0("<font color='",color,"'>",txt,"</font>")
+}
+
+# mark the encoding of character vectors as UTF-8
+mark_utf8 <- function(x) {
+  if (is.character(x)) {
+    Encoding(x) <- 'UTF-8'
+    return(x)
+  }
+  if (!is.list(x)) return(x)
+  attrs <- attributes(x)
+  res <- lapply(x, mark_utf8)
+  attributes(res) <- attrs
+  res
+}
+
+
 # # from and to must be sorted and non-overlapping
 # match.intervals = function(x, from, to) {
 #   from = c(2,5,10.1); to=c(3,8,12)
