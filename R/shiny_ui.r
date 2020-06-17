0cfb5b4c8d3f06b35a444f751ea866fd0f12889d
skranz <sebastian.kranz@uni-ulm.de>
2020-06-17 15:32:44 +0200

    Bugfix Next Exercise Button
    The previous fix to scroll on top caused a bug that not always code
    blocks were rendered. Could not find a pure js solution, so fixed it by
    adding again buttonHandlers in R that call updateTabsetPanel.

--- R/shiny_ui.r
+++ R/shiny_ui.r
@@ -91,10 +91,10 @@ make.ex.ui = function(ex.ind, ps = get.ps(), session=ps$session, view.in.contain
     nextExBtn = simpleButton(btnId,"Go to next exercise...", class.add="nextExBtn")
     #nextExBtn = actionButton(btnId,"Go to next exercise...")
     li = c(li, list(nextExBtn))    
-    # buttonHandler(btnId, ex.ind=ex.ind, function(session=app$session,ex.ind, app=getApp(),...) {
-    #   cat("\nnextExBtn pressed...")
-    #   updateTabsetPanel(session, inputId="exTabsetPanel", selected = paste0("exPanel",ex.ind+1))
-    # })
+    buttonHandler(btnId, ex.ind=ex.ind, function(session=app$session,ex.ind, app=getApp(),...) {
+       cat("\nnextExBtn pressed...")
+       updateTabsetPanel(session, inputId="exTabsetPanel", selected = paste0("exPanel",ex.ind+1))
+    })
   }
   
   do.call("tabPanel", 
--- inst/www/rtutor.js
+++ inst/www/rtutor.js
@@ -1,15 +1,24 @@
 $(document).on('click','.nextExBtn',function(e){
+  
+/*
+  // Unfortunately in the pure java script solution below
+  // Code chunks will not always be shown in the new tab
+  // So I just use the javascript code to scroll up
+  // and change the tabset via a buttonHandler in R
+  
   var id = e.currentTarget.id;
   var curtab  = parseInt(id.substring(9));
   var nexttab = curtab+1;
+  
+  Shiny.setInputValue("exTabsetPanel", "exPanel"+nexttab, {priority: "event"});
 
   var tabsetid = $("#exTabsetPanel").data("tabsetid");
-
   $("#exTabsetPanel li:nth-child("+curtab+")").removeClass('active');
   $("#exTabsetPanel li:nth-child("+nexttab+")").addClass('active');
 
   $("#tab-"+tabsetid+"-"+curtab).removeClass('active');
   $("#tab-"+tabsetid+"-"+nexttab).addClass('active');
+*/
 
   $(window).scrollTop(0);
   //alert('You clicked on the nextExBtn ' + curtab);
