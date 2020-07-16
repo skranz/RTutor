73ba30f509b258d7a6705f2320794bf9209bff00
skranz <sebastian.kranz@uni-ulm.de>
2020-07-16 15:18:26 +0200

    bugfix remove new rmdtools link

--- R/quiz.r
+++ R/quiz.r
@@ -342,8 +342,8 @@ quiz.ui = function(qu, solution=FALSE) {
 }
 
 quiz.part.ui = function(part, solution=FALSE, add.button=!is.null(part$checkBtnId)) {
-  question = rmdtools::md2html(part$question)
-  
+  #question = rmdtools::md2html(part$question)
+  question = markdownToHTML(text=part$question, fragment.only=FALSE,encoding="UTF-8")
   head = list(
     HTML(paste0("<p>",question,"</p>"))
   )
